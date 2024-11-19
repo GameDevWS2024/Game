8d170c7a2df7a12acdbefb73b380cd067e76c72b
Konrad Bausch <dev@konradbausch.com>
2024-11-19 11:49:01 +0100

    modified Ally.cs to make it simpler and to also work with obstacles

--- scripts/Ally.cs
+++ scripts/Ally.cs
@@ -1,159 +1,62 @@
-using System;
-
 using Godot;
 
-namespace Game.Scripts;
-
 public partial class Ally : CharacterBody2D
-{
-    [Export] private NodePath _playerPath;
+    {
+    
+    [Export] private int _minTargetDistance = 300;
+    [Export] private int _targetDistanceVariation = 50;
+    [Export] private int _speed = 250; 
+
+    public bool ReachedTarget {get; private set;}
+    private bool FollowPlayer {get; set;} = true;
+    private NavigationAgent2D _agent;
     private CharacterBody2D _player;
-    private NavigationAgent2D _navigationAgent;
+    private Sprite2D _sprite; 
+
+    private int _currentTargetDistance;
     
-    private const float Speed = 150.0f, DesiredDistance = 150f, AgentRadius = 100f;
-    private Vector2 _velocity = Vector2.Zero;
-    private const double Accel = 2;
-    
-    private readonly Random _ran = new Random();
-    private int _ctr = 0;
-    
-    private Vector2 _playerGlobalPosition, _bounds, _targetPosition;
-    
-    private Boolean _followPlayer = true, _debugPrint = false;
-
-   // public Ally(NodePath playerPath)
-   // {
-    //    _playerPath = playerPath;
-   // }
-
-    public void GoTo(Vector2 loc)
-    {
-        _targetPosition = loc;
-        SetFollowPlayer(false);
-        while (!_navigationAgent.IsTargetReached() || GlobalPosition.DistanceTo(_navigationAgent.GetTargetPosition()) < 50)
-        {
-            _navigationAgent.SetTargetPosition(loc);
-        }
-        DecideWhatNow();
-    }
-
-    private void DecideWhatNow()
-    {
-        if (_followPlayer)
-        {
-            // ...  
-        }
-        // else if (ENEMIES NEARBY) ...
-        // else if (ALREADY BUSY)
-        // else if (RANDOM CHANCE)
-        // else if (TOO FAR FROM PLAYER)
-        else
-        {
-           // SetFollowPlayer(true);
-        }
-    }
-    public void SetFollowPlayer(Boolean follow)
-    {
-        _followPlayer = follow;
-    }
-
-    public Boolean SwitchFollowPlayer()
-    {
-        _followPlayer = !_followPlayer;
-        GD.Print(_followPlayer);
-        return _followPlayer;
-    }
-    public Boolean GetFollowPlayer()
-    {
-        return _followPlayer;
-    }
-
     public override void _Ready()
     {
-        _navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-        _player = GetNode<CharacterBody2D>(_playerPath);
-        _bounds = new Vector2(240, 120);
-        _playerGlobalPosition = _player.GlobalPosition - _bounds;
-        _navigationAgent.PathDesiredDistance = DesiredDistance;
-        _navigationAgent.Radius = AgentRadius;
-        if (_followPlayer)
-        {
-            _navigationAgent.SetTargetPosition(_playerGlobalPosition);
-        }
+        _agent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+        _player = GetNode<CharacterBody2D>("%Player");
+        _sprite = GetNode<Sprite2D>("Sprite2D"); 
+        _currentTargetDistance = _minTargetDistance;
     }
-    public override void _PhysicsProcess(double delta)
+
+    public override void _Process(double delta)
     {
-        _bounds = new Vector2(240, 120); //new Vector2(450, 128);
-        _playerGlobalPosition = _player.GlobalPosition - _bounds;
-        Vector2 nextPosition = _navigationAgent.GetNextPathPosition(); // Get next location to step towards player
-
-        // Update navigation as usual
-        if (_navigationAgent.IsTargetReached() || // if is reached
-            _navigationAgent.GetNextPathPosition().DistanceTo(_playerGlobalPosition) < // or next step would be too far
-            _navigationAgent.PathDesiredDistance &&
-            true) //_navigationAgent.GetNextPathPosition().DistanceTo(GlobalPosition) < 10) // or if it doesnt need to move far (tries to remove jittering around)
+        if (_player == null)
         {
-            _ctr++;
-            _velocity = Vector2.Zero;
-            if (_debugPrint)
-            {
-                GD.Print("Reached position within tolerance - stopping. Updated target position to: " + _playerGlobalPosition + ". Dist: " + _navigationAgent.PathDesiredDistance);
-            }
-
-            if (_ctr > 100) // Element to get a random desired distance to make the following movement feel more natural
-            {
-                _ctr = 0;
-                _navigationAgent.PathDesiredDistance = _ran.Next(180, 200);
-                if (_debugPrint)
-                {
-                    GD.Print(_navigationAgent.PathDesiredDistance + " new des. dist");
-                }
-                if (_followPlayer)
-                {
-                    _navigationAgent.SetTargetPosition(nextPosition);
-                }
-            }
-
-            if (_debugPrint)
-            {
-                GD.Print("Dist: " + GlobalPosition.DistanceTo(_playerGlobalPosition));
-            }
-
-            // if too close back up from player not working yet
-            if (_playerGlobalPosition.DistanceTo(GlobalPosition) < 100)
-            {
-                // _velocity = (nextPosition - GlobalPosition).Normalized() * Speed;  // nextPosition switched with GlobalPosition to invert direction
-                //Velocity = _velocity;
-                //MoveAndSlide();
-            }
-            //
+            return;
         }
-        else
+
+        if(FollowPlayer) {
+            _agent.SetTargetPosition(_player.GlobalPosition);
+        }
+        
+        if (GlobalPosition.DistanceTo(_agent.TargetPosition) > _currentTargetDistance)
         {
-            _velocity = (_playerGlobalPosition - nextPosition).Normalized() * Speed;
-            Velocity = _velocity;
+            ReachedTarget = false;
+            Vector2 curLoc = GlobalPosition;
+            Vector2 nextLoc = _agent.GetNextPathPosition();
+            Vector2 newVel = (nextLoc - curLoc).Normalized() * _speed;
+
+           if (newVel.X != 0){
+            _sprite.FlipH = newVel.X > 0;
+           }
+
+            Velocity = newVel;
             MoveAndSlide();
         }
 
-        //// Now update rotation based on direction to player
-        // Sprite2D allieSprite = GetTree().Root.GetNode<Sprite2D>("TileMapLayer/Region/MeshInstance2D/CharacterBody2D/Sprite2D");
-        // allieSprite.LookAt(_playerGlobalPosition);
-        //GD.Print(allieSprite.GlobalScale);
+        else if (!ReachedTarget){
+            _currentTargetDistance = GD.RandRange(_minTargetDistance - _targetDistanceVariation/2, _minTargetDistance + _targetDistanceVariation/2);
+            ReachedTarget = true;
+        }
     }
-    private void UpdateRotationTowardsPlayer(Vector2 targetPosition, double delta)
-    {
-        float someSmoothingFactor = 3f;
-        Vector2 direction = targetPosition - GlobalPosition;
-        // Calculate angle in radians
-        float angle = Mathf.Atan2(direction.Y, direction.X);
-        // Convert to degrees
-        angle = Mathf.RadToDeg(angle);
-        // Set rotation smoothly (optional)
-        Rotation = Mathf.LerpAngle(Rotation, angle, (float)delta * someSmoothingFactor); // Adjust smoothingFactor for desired speed
-    }
-    private Vector2 GetTargetPosition()
-    {
-        // you could use mouse input or AI to calculate the target.
-        return _targetPosition;
+
+    public void GoTo(Vector2 target) {
+        FollowPlayer = false;
+        _agent.SetTargetPosition(target);
     }
 }

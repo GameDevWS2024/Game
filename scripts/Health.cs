f620a41cb8e1be08e5f366d30221879fc3c03efa
Konrad Bausch <dev@konradbausch.com>
2024-11-23 19:03:50 +0100

    Added Health system, and cleaned up scenes a bit

---
+++ scripts/Health.cs
@@ -0,0 +1,45 @@
+using Godot;
+
+public partial class Health : Node
+{
+    [Signal] public delegate void DeathEventHandler();
+    [Signal] public delegate void HealthChangedEventHandler(int newHealth);
+
+    [Export] private bool _revievable;
+
+    [Export] public double MaxHealth { get; private set; } = 100;
+
+    [Export] public double Amount { get; private set; } = 100;
+    private int _frame = 0;
+
+    public bool Dead { get; private set; } = false;
+
+    public void Heal(double amount)
+    {
+        if ((!Dead || _revievable) && Amount < MaxHealth)
+        {
+            Amount += amount;
+            if (Amount > MaxHealth)
+            {
+                Amount = MaxHealth;
+            }
+
+            EmitSignal(SignalName.HealthChanged, Amount);
+        }
+    }
+
+    public void Damage(double amount)
+    {
+        Amount -= amount;
+
+        if (Amount < 0)
+        {
+            Dead = true;
+            EmitSignal(SignalName.Death);
+            Amount = 0;
+        }
+
+        EmitSignal(SignalName.HealthChanged, Amount);
+    }
+
+}

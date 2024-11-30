57176ad82fea89672b93a6eb0bafe0a1e3622867
Patrick Wölfle <patrickwoelfle3@gmail.com>
2024-11-30 16:39:03 +0100

    Changed Tabs to Spaces

--- scripts/PathFindingMovement.cs
+++ scripts/PathFindingMovement.cs
@@ -1,58 +1,58 @@
 using Godot;
 public partial class PathFindingMovement : Node
 {
-	[Signal] public delegate void ReachedTargetEventHandler();
+    [Signal] public delegate void ReachedTargetEventHandler();
 
-	[Export] private int _minTargetDistance = 150;
-	[Export] private int _targetDistanceVariation = 50;
-	[Export] private int _speed = 250;
-	[Export] bool _debug = false;
+    [Export] private int _minTargetDistance = 150;
+    [Export] private int _targetDistanceVariation = 50;
+    [Export] private int _speed = 250;
+    [Export] bool _debug = false;
 
-	[Export] CharacterBody2D _character = null!;
-	[Export] NavigationAgent2D _agent = null!;
-	[Export] Sprite2D _sprite = null!;
+    [Export] CharacterBody2D _character = null!;
+    [Export] NavigationAgent2D _agent = null!;
+    [Export] Sprite2D _sprite = null!;
 
-	public Vector2 TargetPosition { get; set; }
+    public Vector2 TargetPosition { get; set; }
 
-	private bool _reachedTarget;
-	private int _currentTargetDistance;
+    private bool _reachedTarget;
+    private int _currentTargetDistance;
 
-	public override void _Ready()
-	{
-		_currentTargetDistance = _minTargetDistance;
-	}
+    public override void _Ready()
+    {
+        _currentTargetDistance = _minTargetDistance;
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		_agent.SetTargetPosition(TargetPosition);
+    public override void _PhysicsProcess(double delta)
+    {
+        _agent.SetTargetPosition(TargetPosition);
 
-		if (_debug)
-		{
-			float distance = _character.GlobalPosition.DistanceTo(_agent.TargetPosition);
-			GD.Print($"Distance: {distance}, Target Position: {_agent.TargetPosition}");
-		}
+        if (_debug)
+        {
+            float distance = _character.GlobalPosition.DistanceTo(_agent.TargetPosition);
+            GD.Print($"Distance: {distance}, Target Position: {_agent.TargetPosition}");
+        }
 
-		if (_character.GlobalPosition.DistanceTo(_agent.TargetPosition) > _currentTargetDistance)
-		{
-			_reachedTarget = false;
-			Vector2 currentLocation = _character.GlobalPosition;
-			Vector2 nextLocation = _agent.GetNextPathPosition();
-			Vector2 newVel = (nextLocation - currentLocation).Normalized() * _speed;
+        if (_character.GlobalPosition.DistanceTo(_agent.TargetPosition) > _currentTargetDistance)
+        {
+            _reachedTarget = false;
+            Vector2 currentLocation = _character.GlobalPosition;
+            Vector2 nextLocation = _agent.GetNextPathPosition();
+            Vector2 newVel = (nextLocation - currentLocation).Normalized() * _speed;
 
-			if (newVel.X != 0)
-			{
-				_sprite.FlipH = newVel.X > 0;
-			}
+            if (newVel.X != 0)
+            {
+                _sprite.FlipH = newVel.X > 0;
+            }
 
-			_character.Velocity = newVel;
-			_character.MoveAndSlide();
-		}
-		else if (!_reachedTarget)
-		{
-			_currentTargetDistance = GD.RandRange(_minTargetDistance - _targetDistanceVariation / 2,
-												_minTargetDistance + _targetDistanceVariation / 2);
-			EmitSignal(SignalName.ReachedTarget);
-			_reachedTarget = true;
-		}
-	}
+            _character.Velocity = newVel;
+            _character.MoveAndSlide();
+        }
+        else if (!_reachedTarget)
+        {
+            _currentTargetDistance = GD.RandRange(_minTargetDistance - _targetDistanceVariation / 2,
+                                                _minTargetDistance + _targetDistanceVariation / 2);
+            EmitSignal(SignalName.ReachedTarget);
+            _reachedTarget = true;
+        }
+    }
 }

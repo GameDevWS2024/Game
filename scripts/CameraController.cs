d783f33ca8f7a9dd84f8a0900a951cba05ff26fb
SouperMan <43967698+xSouperMan@users.noreply.github.com>
2025-01-24 14:55:04 +0100

    format3.0

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -4,16 +4,16 @@ using Godot;
 
 public partial class CameraController : Node
 {
-    public static Camera2D _camera1;
-    public static Camera2D _camera2;
+    public static Camera2D Camera1 = null!;
+    public static Camera2D Camera2 = null!;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        _camera1 = GetNode<Camera2D>("Ally1/Ally1Cam");
-        _camera2 = GetNode<Camera2D>("Ally2/Ally2Cam");
+        Camera1 = GetNode<Camera2D>("Ally1/Ally1Cam");
+        Camera2 = GetNode<Camera2D>("Ally2/Ally2Cam");
 
-        _camera1.MakeCurrent();
+        Camera1.MakeCurrent();
 
     }
 

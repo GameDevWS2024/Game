3a1021f3012ff246267b6b5cf642759e8a086539
Luiz <l.simaobento@hotmail.com>
2025-01-31 00:54:32 +0100

    solves Typo

--- scripts/AI/VisibleForAI.cs
+++ scripts/AI/VisibleForAI.cs
@@ -7,19 +7,19 @@ public partial class VisibleForAI : Node2D
 {
     public const string GroupName = "AiVisible";
     [Export] public string NameForAi = "";
-    [Export] public string DescribtionForAi = "";
+    [Export] public string DescriptionForAi = "";
 
     public override void _Ready()
     {
         AddToGroup(GroupName);
     }
 
-    public override string? ToString()
+    public override string ToString()
     {
         if (IsInstanceValid(this))
         {
-            return $"{NameForAi} at ({GlobalPosition.X:F0}, {GlobalPosition.Y:F0}): [Description] {DescribtionForAi}";
+            return $"{NameForAi} at ({GlobalPosition.X:F0}, {GlobalPosition.Y:F0}): [Description] {DescriptionForAi}";
         }
-        return null;
+        return "";
     }
 }
